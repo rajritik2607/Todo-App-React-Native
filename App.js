3c01d2c72bce1871018ab5502b7c03798a37fde0
rajritik2607 <ritik.suryawanshi@gmail.com>
2021-07-16 23:35:50 +0530

    Done

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Button, TextInput } from 'react-native';
+
 
 export default function App() {
+
+  
+
+  const [item, setItems] = useState([{ text: 'Take Pills', key: '1'},
+  { text: 'Drink Tea', key: '2'},
+  { text: 'Buy Groceries', key: '3'},
+  { text: 'Finish Reading the book', key: '4'}])
+
+  var k = 4;
+  var s = String;
+  const showText = () => {
+    //styles.textField.opacity = 
+  }
+
   return (
+    
+
+   <View style = {styles.container}>
+      { <Text style = {styles.headText}> TODO LIST (Note can be deleted by pressing on it) </Text>}
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+       <ScrollView>
+      { 
+      
+      item.map((val)=> {
+        return (
+         
+          <View key = {val.key}>
+            <TouchableOpacity>
+            <Text style={styles.value}>
+              {val.text}
+            </Text>
+            </TouchableOpacity>
+            </View>
+          
+        
+        )
+      })}
+<TextInput onChangeText = {(val) => s(val)}/>
+       </ScrollView>
+       <Button title ='add note' onPress ={showText()} />
+
       <StatusBar style="auto" />
+      
+      
+
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    paddingBottom: 60,
+  },
+  textField: {
+    borderWidth: 1,
+    backgroundColor: 'red'
+  },
+  headText: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
+  value: {
+    padding: 30,
+    borderWidth: 1,
+    color: 'red'
+  }
 });
